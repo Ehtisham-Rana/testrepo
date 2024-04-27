e394d7df1ab179279db59d0c0f452b5403b74a89
Ehtisham-Rana <shamy.rana@gmail.com>
2024-04-27 02:17:37 -0400

    added control flow files

---
+++ control_flow.js
@@ -0,0 +1,47 @@
+//User
+let userRole = "admin";
+let accessLevel;
+// control statement else if
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+//output view
+console.log("Access Level:", accessLevel);
+
+//Login
+let isLoggedIn = true;
+let userMessage;
+// control statement nested if
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+console.log("User Message:", userMessage);
+
+//User Type
+let userType = "subscriber";
+let userCategory;
+// control statement switch
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+console.log("User Category:", userCategory);
