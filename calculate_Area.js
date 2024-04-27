5649c546bb2fb0d6272e0447354524d3ff69e141
Ehtisham-Rana <shamy.rana@gmail.com>
2024-04-27 03:32:01 -0400

    Changed calc_area for Grocery calculations

--- calculate_Area.js
+++ calculate_Area.js
@@ -1,11 +1,14 @@
-let length;
-let width;
+let groc1;
+let groc2;
+let groc3
 
 function calculateArea() {
-    length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value);
-   
-   let area = length * width;
-   document.getElementById('result').innerText = `The area of the rectangle is: ${area}`; }
+    groc1 = parseFloat(document.getElementById('groc1').value);
+    groc2 = parseFloat(document.getElementById('groc2').value);
+    groc3 = parseFloat(document.getElementById('groc3').value);
+
+   let amount = groc1 + groc2 + groc3;
+   document.getElementById('result').innerText = `The area of the rectangle is: ${amount}`; 
+}
    
    
