415895dc59bc7c96d65890cacb204b0a21e58799
Ehtisham-Rana <167960822+Ehtisham-Rana@users.noreply.github.com>
2024-04-27 11:40:38 -0400

    Reset Button added along with Functionality

--- Counter/follow_count.js
+++ Counter/follow_count.js
@@ -14,3 +14,8 @@ function increaseCount() {
           alert("Your Instagram post gained 20 followers! Keep it up!");
         }
       }
+    function resetCount() { 
+        count = 0;
+        displayCount(); // Display the count
+        alert("Counter Reset")
+    }
